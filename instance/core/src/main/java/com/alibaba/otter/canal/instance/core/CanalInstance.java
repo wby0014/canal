8c361bfb03060f6f8ba84dde7c63eaddb9a8c6c1
wubinyu <wuby@weipaitang.com>
2024-11-26 19:26:37 +0800

    add: CanalInstance注释parser-sink-store-meta

--- instance/core/src/main/java/com/alibaba/otter/canal/instance/core/CanalInstance.java
+++ instance/core/src/main/java/com/alibaba/otter/canal/instance/core/CanalInstance.java
@@ -10,7 +10,8 @@ import com.alibaba.otter.canal.store.CanalEventStore;
 
 /**
  * 代表单个canal实例，比如一个destination会独立一个实例
- * 
+ *
+ *  每个实例处理的流程大致是： mysql_master -> canalInstance(伪装slave角色） -> eventParser -> eventSink -> eventStore -> metaManager -> 客户端拉取binlog
  * @author jianghang 2012-7-12 下午12:04:58
  * @version 1.0.0
  */
@@ -18,12 +19,16 @@ public interface CanalInstance extends CanalLifeCycle {
 
     String getDestination();
 
+    // 数据源接入，模拟slave协议和master进行交互，协议解析
     CanalEventParser getEventParser();
 
+    // parser和store链接器，进行数据过滤，加工，分发的工作
     CanalEventSink getEventSink();
 
+    // 数据存储
     CanalEventStore getEventStore();
 
+    // 增量订阅/消费binlog元数据位置存储
     CanalMetaManager getMetaManager();
 
     CanalAlarmHandler getAlarmHandler();
